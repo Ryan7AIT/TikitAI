f2523cf82671266f70dfe28840f0bf2a03f5283a
ryan7ait <r.aitmechedal@datafirst-dz.com>
2025-08-27 16:23:59 +0100

    test langgraph state managment

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from langchain import hub
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langgraph.graph import START, StateGraph
+from langgraph.graph import START, StateGraph,MessagesState
 import re
 from typing_extensions import List, TypedDict
 import glob
@@ -22,13 +22,19 @@ from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
 import time
+from langchain_core.messages import SystemMessage
+from langgraph.prebuilt import ToolNode
+from langchain_core.messages import HumanMessage
+from langgraph.graph import END
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
 
 lolcallm = "llama3.2:latest"
-apillm = "gemma-3-1b-it"
+apillm = "gemini-2.5-flash"
 GOOGLE_API_KEY="AIzaSyDSW8g-DrBwRYEpuBAgjJHCKOYW0rYK0BQ"
 is_local = False
 
-from translator import translate_text
+# from translator import translate_text
 
 
 # print("start translation======================================")
@@ -38,14 +44,14 @@ from translator import translate_text
 # print("Translated:", translated)
 
 
-# if is_local:
-#     llm = ChatOllama(model=lolcallm)
-# else:
-#     llm = init_chat_model(
-#         model=apillm,
-#         model_provider="google_genai",
-#         api_key=GOOGLE_API_KEY,
-#     )
+if is_local:
+    llm = ChatOllama(model=lolcallm)
+else:
+    llm = init_chat_model(
+        model=apillm,
+        model_provider="google_genai",
+        api_key=GOOGLE_API_KEY,
+    )
 
 # embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2") # BAD RESULT EVEN IN ENGLISH
 # use mutlilang model
@@ -163,41 +169,119 @@ class State(TypedDict):
 
 # Define application steps
 
-# def retrieve(state: State):
-#     retrieved_docs = vector_store.similarity_search(state["question"])
-#     return {"context": retrieved_docs}
+@tool(response_format='content_and_artifact')
+def retrieve(query: str):
+    """Search and Retrieve relevant documents from the Knowledge Base to answer user questions."""
+    retrieved_docs = vector_store.similarity_search(query)
+    serialized_docs = "\n\n".join(
+        (f"Source: {doc.metadata}\nContent: {doc.page_content}") for doc in retrieved_docs
+    )
+    return serialized_docs, retrieved_docs
+
+# Step 1: Generate an AIMessage that may include a tool-call to be sent.
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = llm.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    # MessagesState appends messages to state instead of overwriting
+    return {"messages": [response]}
+
+# Step 2: Execute the retrieval.
+tools = ToolNode([retrieve])
+
+# Step 3: Generate a response using the retrieved content.
+def generate(state: MessagesState):
+    """Generate answer."""
+    # Get generated ToolMessages
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    # Format into prompt
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        f"{docs_content}"
+    )
+
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+        or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    # Run
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
 
 
-# def generate(state: State):
+# def generateOLD(state: State):
 #     docs_content = "\n\n".join(doc.page_content for doc in state["context"])
 #     messages = custom_prompt.invoke({"question": state["question"], "context": docs_content})
 #     response = llm.invoke(messages)
 #     return {"answer": response.content}
 
-# # Compile application and test
+# Compile application and test (old style without tools)
 # graph_builder = StateGraph(State).add_sequence([retrieve, generate])
 # graph_builder.add_edge(START, "retrieve")
 # graph = graph_builder.compile()
 
+
+
+
+graph_builder = StateGraph(MessagesState)
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
+
+
 # Test the application
-# response = graph.invoke({"question": "what is overlapping promotions solution?"})
+# response = graph.invoke({"question": "How can i use objective per family"})
 # print(response["answer"])
 
 
+# # Test the application with streaming
+# print("Question: How can i use objective per family")
+# print("Answer: ", end="", flush=True)
 
-start_time = time.time()
-retrieved_docs = vector_store.similarity_search_with_score(
-    # "Comment je peut utiliser  objectif par famille",
-    "How do i use objective per family",
-    # "Hello How are you",
-    # "i am just testing here",
-    k=3
-)
-end_time = time.time()
-print(f"Retrieved {len(retrieved_docs)} documents in {end_time - start_time:.2f} seconds:")
-for doc, score in retrieved_docs:
-    print(f"Document: ----------------------------")
-    print(doc.page_content)
-    print("score: ")
-    print(score)
-    print(f"Document: ----------------------------")
+# for message, metadata  in graph.stream({"question": "How can i use objective per family"}, stream_mode="messages"):
+#     print(message.content, end="|")
+
+
+# input_message = "Hello"
+
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+
+# Specify an ID for the thread
+config = {"configurable": {"thread_id": "a1b2c3"}}
+
+# 
+for step in graph.stream(
+    {"messages": [HumanMessage(content="explain it to me")]},
+    stream_mode="values",
+    config=config,
+):
+    step["messages"][-1].pretty_print()
